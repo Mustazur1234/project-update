709879e70197187329e630887b09be102adc28fb
Mustafizur Rahman <mr2480379@gmail.com>
2021-08-25 13:40:57 +0600

    update

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/common/database/database.module';
+import { AuthModule } from './auth/auth.module';
 import { NoticeModule } from './notice/notice.module';
+import { UserService } from './users/services/user.service';
 import { UsersModule } from './users/user.module';
 
 
+
 @Module({
-  imports: [DatabaseModule, UsersModule, NoticeModule],
-  controllers: [],
-  providers: [],
+  imports: [DatabaseModule, UsersModule, NoticeModule, AuthModule],
+  providers: []
 })
 
 export class ApiModule {}
